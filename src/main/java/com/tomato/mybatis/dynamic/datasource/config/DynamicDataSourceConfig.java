c8ba245e8d615952e8ab64859de91c0199cf5d0e
拔土豆的程序员 <lizhifuabc@163.com>
2023-06-14 22:34:24 +0800

    分库分表初始化配置的数据源

--- src/main/java/com/tomato/mybatis/dynamic/datasource/config/DynamicDataSourceConfig.java
+++ src/main/java/com/tomato/mybatis/dynamic/datasource/config/DynamicDataSourceConfig.java
@@ -8,7 +8,6 @@ import com.tomato.mybatis.dynamic.datasource.properties.DynamicDataSourcePropert
 import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 
@@ -37,11 +36,10 @@ public class DynamicDataSourceConfig {
         // 使用默认数据源的属性配置
         return new HikariDataSource(hikariConfig);
     }
-
     @Bean
     @ConditionalOnProperty(prefix = DynamicDataSourceProperties.PREFIX, name = "mode", havingValue = ShardingConstants.MASTER_SLAVE)
-    public DataSource dataSource() {
-        log.info("初始化配置的数据源");
+    public DataSource masterSlave() {
+        log.info("读写分离初始化配置的数据源");
         DataSource master = createDataSource(properties.getDatasourceMap().get(ShardingConstants.DEFAULT_MASTER));
         DataSource slave = createDataSource(properties.getDatasourceMap().get(ShardingConstants.DEFAULT_SLAVE));
 
@@ -53,6 +51,33 @@ public class DynamicDataSourceConfig {
         Map<Object, Object> targetDataSources = new HashMap<>(16);
         targetDataSources.put(ShardingConstants.DEFAULT_SLAVE, slave);
         dynamicRoutingDataSource.setTargetDataSources(targetDataSources);
+        return dynamicRoutingDataSource;
+    }
+
+    @Bean
+    @ConditionalOnProperty(prefix = DynamicDataSourceProperties.PREFIX, name = "mode", havingValue = ShardingConstants.SHARDING)
+    public DataSource sharding() {
+        log.info("分库分表初始化配置的数据源");
+        Map<String, DynamicDataSourceDetailProperties> datasourceMap = properties.getDatasourceMap();
+        DataSource master = createDataSource(properties.getDatasourceMap().get(ShardingConstants.DEFAULT_MASTER));
+
+        DynamicRoutingDataSource dynamicRoutingDataSource = new DynamicRoutingDataSource();
+        // 设置默认数据源
+        dynamicRoutingDataSource.setDefaultTargetDataSource(master);
+
+        log.info("分库分表初始化配置的数据源,默认数据源key:{}", ShardingConstants.DEFAULT_MASTER);
+        DynamicDataSourceContextHolder.setDataSourceMap(ShardingConstants.DEFAULT_MASTER,master);
+
+        Map<Object, Object> targetDataSources = new HashMap<>(16);
+        datasourceMap.forEach((key, value) -> {
+            log.info("分库分表初始化配置的数据源key:{}:value{}",key,value);
+            DataSource target = createDataSource(value);
+            DynamicDataSourceContextHolder.setDataSourceMap(key,target);
+            targetDataSources.put(key, target);
+        });
+        // 添加数据源
+        dynamicRoutingDataSource.setTargetDataSources(targetDataSources);
+
         return dynamicRoutingDataSource;
     }
 }
