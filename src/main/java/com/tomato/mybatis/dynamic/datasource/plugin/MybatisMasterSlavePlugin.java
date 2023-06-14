e1d280a3ceb232e5cedda7c699af9b138f53d519
拔土豆的程序员 <lizhifuabc@163.com>
2023-06-14 17:10:51 +0800

    [新增] 读写分离插件

---
+++ src/main/java/com/tomato/mybatis/dynamic/datasource/plugin/MybatisMasterSlavePlugin.java
@@ -0,0 +1,48 @@
+package com.tomato.mybatis.dynamic.datasource.plugin;
+
+import com.tomato.mybatis.dynamic.datasource.constant.ShardingConstants;
+import com.tomato.mybatis.dynamic.datasource.context.DynamicDataSourceContextHolder;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.ibatis.cache.CacheKey;
+import org.apache.ibatis.executor.Executor;
+import org.apache.ibatis.mapping.BoundSql;
+import org.apache.ibatis.mapping.MappedStatement;
+import org.apache.ibatis.mapping.SqlCommandType;
+import org.apache.ibatis.plugin.*;
+import org.apache.ibatis.session.ResultHandler;
+import org.apache.ibatis.session.RowBounds;
+
+import java.util.Properties;
+
+/**
+ * 读写分离插件
+ * @author lizhifu
+ */
+@Intercepts({
+        @Signature(type = Executor.class, method = "query", args = {MappedStatement.class, Object.class, RowBounds.class, ResultHandler.class}),
+        @Signature(type = Executor.class, method = "query", args = {MappedStatement.class, Object.class, RowBounds.class, ResultHandler.class, CacheKey.class, BoundSql.class}),
+        @Signature(type = Executor.class, method = "update", args = {MappedStatement.class, Object.class})})
+@Slf4j
+public class MybatisMasterSlavePlugin implements Interceptor {
+    @Override
+    public Object intercept(Invocation invocation) throws Throwable {
+        Object[] args = invocation.getArgs();
+        MappedStatement ms = (MappedStatement) args[0];
+        try {
+            String dataSource = SqlCommandType.SELECT == ms.getSqlCommandType() ? ShardingConstants.DEFAULT_SLAVE : ShardingConstants.DEFAULT_MASTER;
+            DynamicDataSourceContextHolder.setDatasource(dataSource);
+            return invocation.proceed();
+        } finally {
+            DynamicDataSourceContextHolder.clearDatasource();
+        }
+    }
+
+    @Override
+    public Object plugin(Object target) {
+        return Plugin.wrap(target, this);
+    }
+
+    @Override
+    public void setProperties(Properties properties) {
+    }
+}
