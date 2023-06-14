55bc1f3c26030ee5efc9bb7a26dd57f9b5d48d73
拔土豆的程序员 <lizhifuabc@163.com>
2023-06-14 17:21:07 +0800

    [新增] 动态数据源配置

--- src/main/java/com/tomato/mybatis/dynamic/datasource/config/DynamicDataSourceAutoConfig.java
+++ src/main/java/com/tomato/mybatis/dynamic/datasource/config/DynamicDataSourceAutoConfig.java
@@ -1,12 +1,15 @@
 package com.tomato.mybatis.dynamic.datasource.config;
 
+import com.tomato.mybatis.dynamic.datasource.constant.ShardingConstants;
+import com.tomato.mybatis.dynamic.datasource.plugin.MybatisDynamicDatasourcePlugin;
+import com.tomato.mybatis.dynamic.datasource.plugin.MybatisMasterSlavePlugin;
 import com.tomato.mybatis.dynamic.datasource.properties.DynamicDataSourceProperties;
 import org.springframework.boot.autoconfigure.AutoConfiguration;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
 
-import javax.sql.DataSource;
 
 /**
  * 动态数据源配置
@@ -18,5 +21,22 @@ import javax.sql.DataSource;
 @EnableConfigurationProperties(DynamicDataSourceProperties.class)
 @Import({DynamicDataSourceConfig.class})
 public class DynamicDataSourceAutoConfig {
-    
+    /**
+     * 读写分离插件
+     * @return 读写分离插件
+     */
+    @Bean
+    @ConditionalOnProperty(prefix = DynamicDataSourceProperties.PREFIX, name = "mode", havingValue = ShardingConstants.MASTER_SLAVE)
+    MybatisMasterSlavePlugin mybatisMasterSlavePlugin() {
+        return new MybatisMasterSlavePlugin();
+    }
+    /**
+     * 分库分表插件
+     * @return 分库分表插件
+     */
+    @Bean
+    @ConditionalOnProperty(prefix = DynamicDataSourceProperties.PREFIX, name = "mode", havingValue = ShardingConstants.SHARDING)
+    MybatisDynamicDatasourcePlugin mybatisDynamicDatasourcePlugin() {
+        return new MybatisDynamicDatasourcePlugin();
+    }
 }
